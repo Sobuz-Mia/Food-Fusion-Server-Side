a76b14d54440e4af2663a93a2752d473882918db
Sobuz <sobuz.info0298@gmail.com>
2023-11-06 02:52:38 +0600

    get all food item from database and show ui

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express());
 
 
-
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.sbw5eqf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,7 +33,6 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result)
     })
-
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
