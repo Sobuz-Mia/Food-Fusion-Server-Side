fc80a83b70e0f4a992887114b8ae03339beee653
Sobuz <sobuz.info0298@gmail.com>
2023-11-06 12:57:39 +0600

    api create for post food

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 var cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.sbw5eqf.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,10 +27,24 @@ async function run() {
     // await client.connect();
 
     const featuresCollection = client.db('communityFoodDB').collection('allFeatures');
+    const foodsCollection = client.db('communityFoodDB').collection('foods');
 
     app.get('/api/v1/all-features',async (req,res)=>{
         const cursor = featuresCollection.find();
         const result = await cursor.toArray();
+        res.send(result)
+    })
+    app.get('/api/v1/food-Details/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = { _id : new ObjectId(id)}
+        const result =await featuresCollection.findOne(query)
+        res.send(result)
+    })
+
+    app.post('/api/v1/foods',async(req,res)=>{
+        const food = req.body;
+        console.log(food)
+        const result = await foodsCollection.insertOne(food);
         res.send(result)
     })
     // Send a ping to confirm a successful connection
